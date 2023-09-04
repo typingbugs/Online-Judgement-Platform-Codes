205dbd06fda9680d62d8ad89ae6df1301af07252
kejingfan <jingfan_ke@bjtu.edu.cn>
2023-09-04 11:47:18 +0800

    refactor project

--- Luogu/P2392/cmake-build-debug/cmake_install.cmake
+++ Luogu/P2392/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/Code/Online-Judgement-Platform-Codes/Luogu/P2392
+# Install script for directory: F:/Code/Online-Judgement-Platform-Codes/Luogu/P2392
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/Code/Online-Judgement-Platform-Codes/Luogu/P2392/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "F:/Code/Online-Judgement-Platform-Codes/Luogu/P2392/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Luogu/P5704/P5704_字母转换.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	char input;
-	scanf("%c", &input);
-	printf("%c\n", input - 'a' + 'A');
-	return 0;
-}
