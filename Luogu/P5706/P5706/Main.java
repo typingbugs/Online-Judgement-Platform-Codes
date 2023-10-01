8349b41bdb5a5c2c53338fac2d7dacf0345d8552
kejingfan <jingfan_ke@bjtu.edu.cn>
2023-10-01 22:36:08 +0800

    refactor project, problem: P5706 再分肥宅水

--- Luogu/P5706/P5706/Main.java
+++ Luogu/P5706/P5706/Main.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
-public class Main
-{
-	public static void main(String[] args)
-	{
+public class Main {
+	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		double t = scan.nextDouble();
 		int n = scan.nextInt();
