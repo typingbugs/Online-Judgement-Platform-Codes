87d3accb1354698c3cbfa7cde24a24b470f19575
kejingfan <jingfan_ke@bjtu.edu.cn>
2023-10-12 12:32:38 +0800

    删除除代码以外的文件

--- BJTU OJ/2023.2 第十一届新生程序设计竞赛及蓝桥杯选拔赛/H_cats_的随机化做题策略.py
+++
@@ -1,6 +0,0 @@
-n = input()
-ans = 0
-inp = input().split()
-for _ in inp:
-    ans += 100 / eval(_) * 20 - 20
-print(ans)
--- Codeforces/Codeforces Round 898 (Div. 4)/A.py
+++
@@ -1,6 +0,0 @@
-ans = {'abc', 'acb', 'bac', 'cba'}
-
-t = int(input())
-while t != 0:
-    t -= 1
-    print('YES' if input() in ans else 'NO')
